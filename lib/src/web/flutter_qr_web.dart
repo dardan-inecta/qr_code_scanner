ca0bbce134e746664adaacd7aa6748947f89728a
Sweta - Inecta <151121857+swetav1996@users.noreply.github.com>
2025-10-09 16:50:57 +0530

    Update flutter_qr_web.dart

--- lib/src/web/flutter_qr_web.dart
+++ lib/src/web/flutter_qr_web.dart
@@ -10,6 +10,7 @@ import 'web_view_registry.dart';
 import 'package:flutter/material.dart';
 
 import '../../qr_code_scanner.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart' as web_plugins;
 import 'jsqr.dart';
 import 'media.dart';
 
@@ -76,10 +77,10 @@ class _WebQrViewState extends State<WebQrView> {
     // video = html.VideoElement();
     WebQrView.vidDiv.children = [video];
     // ignore: undefined_prefixed_name
-    platformViewRegistry.registerViewFactory(
-      viewID,
-      (int id) => WebQrView.vidDiv,
-    );
+    web_plugins.registerViewFactory(
+  viewID,
+  (int id) => WebQrView.vidDiv,
+);
 
     print('[QR Scanner Web] viewID: $viewID');
     
